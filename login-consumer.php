80d4cc9f2eba6ce8f4a4a2bcc1b00917497404b5
burgu-874444 <114607466+burgu-874444@users.noreply.github.com>
2022-10-23 22:08:54 +0530

    Add files via upload
    Login/Logout feature

---
+++ login-consumer.php
@@ -0,0 +1,45 @@
+<?php require_once "controllerUserData.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Consumer Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form login-form">
+                <form action="login-consumer.php" method="POST" autocomplete="">
+                    <h2 class="text-center">Consumer Login</h2>
+                    <p class="text-center">Use Email Address As User ID.</p>
+                    <?php
+                    if(count($errors) > 0){
+                        ?>
+                        <div class="alert alert-danger text-center">
+                            <?php
+                            foreach($errors as $showerror){
+                                echo $showerror;
+                            }
+                            ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                    <div class="form-group">
+                        <input class="form-control" type="email" name="email" placeholder="User ID" required value="<?php echo $email ?>">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="password" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="login" value="Login">
+                    </div>
+                    <div class="link login-link text-center">Not a Consumer? <a href="signup-consumer.php">Signup Here</a></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
